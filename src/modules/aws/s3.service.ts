226c3c9d8b9c196a0eee4dabdfed6af551733df1
Victor Santos <menezes.victorsantos@outlook.com>
2026-07-10 06:54:59 -0300

    refactor: change bucket provider from S3 to minIO.
    MinIO uses the same s3 api from AWS, no big changes were needed.
    
    this commit also includes the following:
    - Created minIO containers `minio` and `createbucket`
    - Added enviroment variables checks at `config/env.ts`
    -  `s3.service.ts` refactored to handle minIO bucket configurations.

--- src/modules/aws/s3.service.ts
+++ src/modules/aws/s3.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3"
-import { fromIni } from "@aws-sdk/credential-providers"
 import { ConfigService } from "@nestjs/config";
-import { env, Env } from "../../config/env";
+import { Env } from "../../config/env";
 import { GetUploadUrlParams } from "./types";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 @Injectable()
@@ -10,8 +9,13 @@ export class S3Service extends S3Client {
     private readonly bucket: string;
     constructor(private readonly configService: ConfigService<Env>) {
         super({
-            // Reads credentials from Ini if on dev environment
-            credentials: configService.getOrThrow("NODE_ENV") == "production" ? undefined : fromIni({ profile: "saturn" }),
+            // Using minIO as default bucket
+            credentials: {
+                accessKeyId: configService.getOrThrow("MINIO_USER"),
+                secretAccessKey: configService.getOrThrow("MINIO_PASSWORD")
+            },
+            endpoint: configService.getOrThrow("S3_ENDPOINT"),
+            forcePathStyle: true,
             region: "sa-east-1"
         })
         this.bucket = this.configService.getOrThrow("BUCKET")
